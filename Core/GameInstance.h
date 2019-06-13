e4583173fca65022d2118d0a3c8a999b6658f728
jjr2930 <joojungyeol@gmail.com>
2019-06-13 11:54:56 +0900

    업데이트 중....
    새로운 변경사항 업데이트 중..

--- Core/GameInstance.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Object.h"
-namespace JEngine
-{
-    class GameInstance : public Object
-    {
-    
-    };
-}
--- JConst.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-using UINT      = unsigned __int32;
-using UINT16    = unsigned __int16;
-using UINT8     = unsigned __int8;
-
-const char LOG_FILE_NAME[] = "Log.txt";
-
-
